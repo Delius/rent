279a1a488060bccf7f8c175d1328fde1d06e7e38
Delius <pawel@fibrotrust.org>
2014-11-27 19:48:47 +0000

    adsd

--- app/models/property.rb
+++ app/models/property.rb
@@ -4,8 +4,10 @@ belongs_to :user
 before_save  :year_return, :year_return_percent
 before_create  :year_return, :year_return_percent
 
-before_validation :upcase_postCode
-after_validation :shorten_postCode
+before_validation :upcase_post_code
+before_validation :shorten_post_code
+
+
 
 
 def year_return
@@ -18,13 +20,13 @@ def year_return_percent
 
 end
 
-def upcase_postCode
+def upcase_post_code
 
   self.postCode = self.postCode.upcase
 
 end
 
-def shorten_postCode
+def shorten_post_code
 
   self.postCode = self.postCode.gsub(/\s.*/, '')
 
