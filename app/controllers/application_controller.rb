6f8905d1b0d80326859568e9a4a88f55cf277686
Delius <pawel@fibrotrust.org>
2014-12-01 03:56:38 +0000

    add comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :set_cache_buster
-
+# deletes cache after logout
   def set_cache_buster
     response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
   end
-
+# fetches similar properties based on place name of nestoria api
   def similar
     @property = Property.find(params[:id])
     @response = HTTParty.get("http://api.nestoria.co.uk/api?place_name=#{@property.postCode}&action=search_listings&pretty=1&encoding=json&listing_type=rent&country=uk")
