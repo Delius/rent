843f5859198ef28796a6d1ef483593e1fdcff7f3
Delius <pawel@fibrotrust.org>
2014-12-01 03:35:04 +0000

    add validations

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,6 +1,8 @@
 class Property < ActiveRecord::Base
   belongs_to :user
-
+  validates :propertyCost, :numericality => { :greater_than_or_equal_to => 0 ,message: "only positive numbers" }
+  validates :propertyRent, :numericality => { :greater_than_or_equal_to => 0,message: "only positive numbers" }
+  validates :phone_nr, format:{ with:  /\d{3}-\d{3}-\d{4}/,message: "bad format" }
   before_save  :year_return, :year_return_percent
   before_create  :year_return, :year_return_percent
 
